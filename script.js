8988eae3e4f8de9da3cfa2be7530adf064b83b0e
Uju Somto <officialdustman@gmail.com>
2022-09-07 16:42:32 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const list = document.querySelectorAll('ul li'),
+      nav = document.querySelector('ul'),
+      bar = document.querySelector('.icons > :first-child');
+
+list.forEach((lists) => {
+
+    lists.onclick = function(){
+        
+        for (let i = 0;  i < list.length; i++) {
+            if (list[i].classList = 'li-active') {
+                list[i].classList.remove('li-active')
+            }
+        }
+
+        lists.classList.add('li-active')
+    }
+  });
+
+bar.onclick = function(){
+
+    bar.classList.toggle('fa-xmark')
+
+    if (nav.style.display === 'none') {
+        nav.style.display = 'flex'   
+    } else {
+        nav.style.display = 'none'
+    }
+}
+
+// let swiper = new Swiper("#home", {
+//     pagination: {
+//       el: ".swiper-pagination",
+//       dynamicBullets: true,
+//     },
+// });
+
+let swip = new Swiper(".swiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+});
